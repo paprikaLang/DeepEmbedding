a4526277e01a6601960a200173c90e259bdc559d
paprikaLang <3060866223@qq.com>
2018-01-20 02:25:32 +0800

    add struct embedding

--- DeepEmbedding/ViewController.swift
+++ DeepEmbedding/ViewController.swift
@@ -10,26 +10,31 @@ import UIKit
 
 final class AnalyticsBackend {
     static let shared = AnalyticsBackend()
-    func send(event: String, metadata: [String:String]) {
-        print("Sending event \(event) , metadata: \(metadata)")
+    func send(_ event: Event) {
+        print("Sending event \(event.name) , metadata: \(event.metadata)")
     }
 }
-extension AnalyticsBackend {
-    func tapMeTapped() {
-        send(event: "viewController.tapMeTapped", metadata: [:])
+
+struct Event {
+    let name: String
+    let metadata: [String:String]
+}
+extension Event {
+    static func tapMeTapped() -> Event {
+        return Event(name:"viewController.tapMeTapped",metadata:[:])
     }
-    func viewWillAppear(name: String) {
-        send(event: "viewController.appear", metadata: ["name":name,"time":"\(Date().timeIntervalSince1970)"])
+    static func viewWillAppear(name: String) -> Event{
+        return Event(name:"viewController.appear", metadata: ["name":name,"time":"\(Date().timeIntervalSince1970)"])
     }
 }
 class ViewController: UIViewController {
     @IBAction func tapMeTapped(_ sender: Any) {
-        AnalyticsBackend.shared.tapMeTapped()
+        AnalyticsBackend.shared.send(.tapMeTapped())
     }
     
    override  func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        AnalyticsBackend.shared.viewWillAppear(name: "master")
+        AnalyticsBackend.shared.send(.viewWillAppear(name: "master"))
     }
 
 }
@@ -38,6 +43,6 @@ class ViewController: UIViewController {
 class DetailController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        AnalyticsBackend.shared.viewWillAppear(name: "detail")
+        AnalyticsBackend.shared.send(.viewWillAppear(name: "detail"))
     }
 }
--- DeepEmbeddingTests/DeepEmbeddingTests.swift
+++ DeepEmbeddingTests/DeepEmbeddingTests.swift
@@ -22,8 +22,8 @@ class DeepEmbeddingTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+       let e = Event.viewWillAppear(name: "test")
+        XCTAssertEqual(e.metadata["name"], "test")
     }
     
     func testPerformanceExample() {
